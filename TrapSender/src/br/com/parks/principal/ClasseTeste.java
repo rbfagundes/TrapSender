be071bb951ea7371ebc042d151158e6aa2f02db8
fagundes <fagundes@fagundes-PC>
2014-10-08 09:24:53 -0300

    Commitando classe de teste

---
+++ TrapSender/src/br/com/parks/principal/ClasseTeste.java
@@ -0,0 +1,19 @@
+package br.com.parks.principal;
+
+public class ClasseTeste {
+	private String nome;
+	private int idade;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+}
