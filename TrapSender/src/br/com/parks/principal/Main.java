2901a2a35db24cce38269aa522900c279c312b63
rfagundes <Rodrigo@Rodrigo-PC>
2014-10-08 15:11:21 -0300

    Commit na branch bugID4884

--- TrapSender/src/br/com/parks/principal/Main.java
+++ TrapSender/src/br/com/parks/principal/Main.java
@@ -6,12 +6,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		new TrapSenderPanel().setVisible(true);
-		
-		// alteração na classe main.
-		
-		// segunda alteração
-		
-		// terceira alteração.
 	}
 
 }
